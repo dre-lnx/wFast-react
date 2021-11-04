c9f81db8bbb2cbd090a87070ddf5de59c39f7bf2
Andre Santos <andre.230303@bento.local>
2021-11-04 11:41:33 -0300

    tela-login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import * as React from 'react';
 import Button from '@material-ui/core/Button';
-import './App.css';
-
-
+import './assets/App.css';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
 
 function App() {
   return (
     <Button variant="contained" color="primary">
       Hello World
-    </Button>
-
+    </Button>,
+    <Container maxWidth="xs" className="login-container ">
+          <form noValidate autoComplete="off">
+            <div class="form-content">
+                <TextField id="outlined-basic" size="small" label="Outlined" variant="outlined"/>
+                <TextField id="outlined-basic" size="small"  label="Outlined" variant="outlined"/>
+                <Button variant="contained"  size="small" color="primary">
+                  Sign In
+                </Button>
+                <Button variant="contained"  size="small" color="success">
+                  Sign Up
+                </Button>
+            </div>
+          </form>
+    </Container>
   );
 }
 
---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Container from '@material-ui/core/Container';
+import '../assets/App.css';
+
+const Login = () => {
+    return (
+        <Container maxWidth="sm">
+            sada
+        </Container>
+    )
+}

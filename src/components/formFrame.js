ccd84e962892f566e6c343b73903390e9d4e236f
Andre Santos <andre.230303@bento.local>
2021-11-11 10:33:44 -0300

    add signUp form

--- src/components/formFrame.js
+++ src/components/formFrame.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import logo from '../assets/logo.svg';
 import ReactDOM from 'react-dom';
 
@@ -6,11 +6,12 @@ const FormFrame = () => {
 
     const[form, setForm] = useState(false);
 
-    const setSignForm = () => {
-            if(form === true) {
-                const element = document.querySelector('#signIn-form');
-                element.classList.add('animate__animated', 'animate__bounceOutLeft');
-            }
+    useEffect(() => {
+        console.log(form);
+    }, [form])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
     }
 
     return (
@@ -18,23 +19,31 @@ const FormFrame = () => {
     
     
     <div className="login-container-sm">
-        <form>
-            <div className="form-y-layout" id="signIn-form">
+        <form onSubmit = {(e) => handleSubmit(e)}>
+                <div className="form-y-layout" id="signIn-form">
                 <div className="header">
                     <img src={logo} alt="wfast logo" className="formLogo"/>
                 </div>
+            {
+                form === false &&
+                <>
                 <input type="text" placeholder="E-mail ou usuário"/>
                 <input type="password" placeholder="Senha"/>
                 <input type="submit" value="Entrar"/>
                 <button className="form-link" onClick={() => {setForm(true)}}>Ainda não tem uma conta?</button>
                 <button className="form-link">Esqueci minha senha</button>
+            </>
+            }
             </div>
+            {form === true &&
             <div className="form-y-layout" id="signUp-form">
                 <input type="text" placeholder="Nome de usuário"/>
                 <input type="text" placeholder="E-mail"/>
                 <input type="password" placeholder="Senha"/>
                 <input type="submit" value="Criar Conta"/>
+                <button className="form-link"  onClick={() => {setForm(false)}}>Já possuo uma conta</button>
             </div>
+            }
         </form>
     </div>
     );

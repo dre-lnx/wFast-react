920d1034fc45da7e5460dc6d6b8661b999f8e991
Andre Santos <andre.230303@bento.local>
2021-11-11 11:42:49 -0300

    dashboard start

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from 'react'
 
 const Dashboard = () => {
-    return (
-        <div className="dashboard-container">
-            Supposed to be Dashboard
-            <h1 className="animate__animated animate__bounce">An animated element</h1>
+  return (
+    <div className="dashboard-container">
+      <div className="boards-container">
+        <div className="dashboard-sidebar">
+          <div className="dashboard-header">
+            <h3>Boards</h3>
+          </div>
+          <ul className="sidebar-list-container">
+            <li>
+              <i class="fas fa-plus"></i>Create Board
+            </li>
+          </ul>
         </div>
-    );
+        <div className="dashboard-body">
+          <div className="dashboard-header">
+            <h3>Bem vindo, Rick</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Dashboard;
+export default Dashboard

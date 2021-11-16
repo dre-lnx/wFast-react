4624aea00e92c06a072a134120dfbe60c001e124
Andre Santos <andre.230303@bento.local>
2021-11-16 10:56:46 -0300

    dashboard done?

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,22 +1,38 @@
 import React from 'react'
+import logo from '../assets/rick.jpg'
 
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
       <div className="boards-container">
         <div className="dashboard-sidebar">
-          <div className="dashboard-header">
-            <h3>Boards</h3>
+          <div className="sidebarContainer">
+            <div className="dashboard-profile">
+              <img src={logo} className="dashboard-avatar" />
+              <h4>Ricky Astley</h4>
+              <span>ricky69@gmail.com</span>
+            </div>
           </div>
-          <ul className="sidebar-list-container">
-            <li>
-              <i class="fas fa-plus"></i>Create Board
-            </li>
-          </ul>
         </div>
         <div className="dashboard-body">
           <div className="dashboard-header">
-            <h3>Bem vindo, Rick</h3>
+            <h1>Boards</h1>
+            <div className="divider"></div>
+          </div>
+          <div className="boardsBody">
+            <div className="boardCard addBoard">
+              <span>Criar Board</span>
+              <i class="fas fa-plus fa-2x"></i>
+            </div>
+            <div className="boardCard">
+              <span>Título da board</span>
+            </div>
+            <div className="boardCard">
+              <span>Título da board</span>
+            </div>
+            <div className="boardCard">
+              <span>Título da board</span>
+            </div>
           </div>
         </div>
       </div>

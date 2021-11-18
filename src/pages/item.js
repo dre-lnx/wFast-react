162dd2da92bfe1145f981ab3519f7cf92a7ceaae
Andre Santos <andre.230303@bento.local>
2021-11-18 11:00:39 -0300

    board tasks

--- src/pages/item.js
+++ src/pages/item.js
@@ -6,23 +6,32 @@ const Item = () => {
       <div className="status toDo">
         <div className="status-header">
           <span>To do</span>
-          <i class="fas fa-ellipsis-h"></i>
         </div>
-        <div className="status-body">asd</div>
+        <div className="status-body task">
+          asd<i class="fas fa-ellipsis-h"></i>
+        </div>
+        <div className="plus-item">
+          <i class="fas fa-plus-circle"></i>
+        </div>
       </div>
       <div className="status doing">
         <div className="status-header">
           <span>Doing</span>
-          <i class="fas fa-ellipsis-h"></i>
         </div>
-        <div className="status-body">asd</div>
+        <div className="status-body task">
+          asd<i class="fas fa-ellipsis-h"></i>
+        </div>
+        <div className="plus-item">
+          <i class="fas fa-plus-circle"></i>
+        </div>
       </div>
       <div className="status done">
         <div className="status-header">
           <span>Done</span>
-          <i class="fas fa-ellipsis-h"></i>
         </div>
-        <div className="status-body">asd</div>
+        <div className="status-body task">
+          asd<i class="fas fa-ellipsis-h"></i>
+        </div>
         <div className="plus-item">
           <i class="fas fa-plus-circle"></i>
         </div>

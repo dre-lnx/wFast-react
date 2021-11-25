e6af20c23def6432db311c42dbe030a8b7f0abfe
Andre Santos <andre.230303@bento.local>
2021-11-25 11:04:04 -0300

    animations mods

--- src/pages/item.js
+++ src/pages/item.js
@@ -2,35 +2,43 @@ import React from 'react'
 
 const Item = () => {
   return (
-    <div className="item-container">
-      <div className="status toDo">
-        <div className="status-header">
-          <span>To do</span>
-          <i class="fas fa-ellipsis-h"></i>
-        </div>
-        <div className="status-body task">asd</div>
-        <div className="plus-item">
-          <i class="fas fa-plus-circle"></i>
+    <div className="board-container">
+      <div className="board-header">
+        <div className="board-title">
+          <i class="fas fa-sort-down"></i>
+          <h3>Título da board</h3>
         </div>
       </div>
-      <div className="status doing">
-        <div className="status-header">
-          <span>Doing</span>
-          <i class="fas fa-ellipsis-h"></i>
+      <div className="item-container">
+        <div className="status toDo">
+          <div className="status-header">
+            <span>To do</span>
+            <i class="fas fa-ellipsis-h"></i>
+          </div>
+          <div className="status-body task">asd</div>
+          <div className="plus-item">
+            <i class="fas fa-plus-circle"></i>
+          </div>
         </div>
-        <div className="status-body task">asd</div>
-        <div className="plus-item">
-          <i class="fas fa-plus-circle"></i>
+        <div className="status doing">
+          <div className="status-header">
+            <span>Doing</span>
+            <i class="fas fa-ellipsis-h"></i>
+          </div>
+          <div className="status-body task">asd</div>
+          <div className="plus-item">
+            <i class="fas fa-plus-circle"></i>
+          </div>
         </div>
-      </div>
-      <div className="status done">
-        <div className="status-header">
-          <span>Done</span>
-          <i class="fas fa-ellipsis-h"></i>
-        </div>
-        <div className="status-body task">asd</div>
-        <div className="plus-item">
-          <i class="fas fa-plus-circle"></i>
+        <div className="status done">
+          <div className="status-header">
+            <span>Done</span>
+            <i class="fas fa-ellipsis-h"></i>
+          </div>
+          <div className="status-body task">asd</div>
+          <div className="plus-item">
+            <i class="fas fa-plus-circle"></i>
+          </div>
         </div>
       </div>
     </div>

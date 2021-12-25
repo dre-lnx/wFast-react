61f5d2bb5c7f40c92613324fbd35b11c5d5bc0aa
dre-lnxd <andrevlds23@gmail.com>
2021-12-25 19:29:30 -0300

    avatar dropdown

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import logo from '../assets/only_logo.svg'
@@ -6,34 +6,54 @@ import logo from '../assets/only_logo.svg'
 import avatar from '../assets/rick.jpg'
 
 const NavBar = () => {
-  return (
-    <div className="nav-container">
-      <i class="fas fa-bars fa-2x"></i>
-      <nav className="a-list">
-        <div>
-          <img src={logo} alt="logo wfast" className="nav-logo" />
-        </div>
-        <ul>
-          <li>
-            <Link to="/Login">Login</Link>
-          </li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/item">Item</Link>
-          </li>
-        </ul>
-      </nav>
+  const [dropDown, setDropdown] = useState(false)
 
-      <div className="avatar-div">
-        <span className="avatar-title">Rick</span>
-        <img src={avatar} alt="user avatar" className="avatar-img" />
+  const toggle = (val) => {
+    val === true ? setDropdown(false) : setDropdown(true)
+  }
+
+  return (
+    <>
+      <div className="nav-container">
+        <i class="fas fa-bars fa-2x"></i>
+        <nav className="a-list">
+          <div>
+            <img src={logo} alt="logo wfast" className="nav-logo" />
+          </div>
+          <ul>
+            <li>
+              <Link to="/Login">Login</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/item">Item</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="avatar-div" onClick={() => toggle(dropDown)}>
+          <span className="avatar-title">Rick</span>
+          <img src={avatar} alt="user avatar" className="avatar-img" />
+        </div>
       </div>
-    </div>
+      {dropDown === true && (
+        <div className="dropDown flex-x-end">
+          <ul>
+            <li>
+              <i class="fas fa-sign-out-alt"></i>Log out
+            </li>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
   )
 }
 

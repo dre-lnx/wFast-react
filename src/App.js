59761ee5392266b42e62501484cc36915b7314dc
dre-lnxd <andrevlds23@gmail.com>
2022-01-24 08:34:52 -0300

    setando apollo client

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import * as React from 'react'
 import './assets/App.css'
 import AppRouter from './routes'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from '@apollo/client'
+
+import { ErrorLink, onError } from '@apollo/client/link/error'
+
+const errorLink = onError(({ graphqlErrors, networkErrors }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql Error ${message}`)
+    })
+  }
+})
+const link = from([
+  ErrorLink,
+  new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+])
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+})
 
 function App() {
-  return <AppRouter />
+  return (
+    <ApolloProvider client={client}>
+      <AppRouter />
+    </ApolloProvider>
+  )
 }
 
 export default App
